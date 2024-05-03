1a2901b209b6cafd18ef700a180717130bd8957d
Sudipta Basak <sudiptab2100@gmail.com>
2024-05-03 18:12:52 +0530

    multiaddr update

--- multiaddr/multiaddr.py
+++ multiaddr/multiaddr.py
@@ -3,15 +3,17 @@ import multiaddr.validator as vd
 class MultiAddr:
     def __init__(self, maddr: str):
         """
-        Constructor for the Multiaddr class.
-        
         Args:
             maddr (str): The multiaddress string in this '/ip4/10.10.10.10/tcp/62535/QmSudipta' format
         """
-        # Initialize the Multiaddr object with the given multiaddress string
         self.maddr = maddr
         if not self.isValid(maddr):
             raise ValueError("Invalid multiaddress")
+        
+        self.ipVersion, self.ipAddr, self.transport, self.port, self.peerId = list(maddr[1: ].split('/'))
+    
+    def __str__(self):
+        return self.maddr
     
     def isValid(self, maddr):
         ipVersion, ipAddr, transport, port, peerId = list(maddr[1: ].split('/'))
